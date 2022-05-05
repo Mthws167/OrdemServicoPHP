d16420356ad0bc0b9c839de7c36d5fee3559edad
Matheus Henrique de Oliveria <62122558+Mthws167@users.noreply.github.com>
2022-05-05 20:59:28 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="pt-BR">
 
@@ -18,60 +17,40 @@
 
 </head>
 
-<body class="box"onsubmit="return false">
+<body class="box">
     <div class="cards" id="form1">
-        <form  onsubmit="resultado()"  method="post">
-        <b>Ordem de Serviço</b>
+        <form action="validar.php" onsubmit="resultado()" method="post">
+            <b>Ordem de Serviço</b>
             <div>
                 <h2>Nº da O.S:</h2>
-                <input type="text" name="nos" id="nos" class="Caixa-Texto-N-OS" required="required"/>
-                <script>var evento = evt || window.event;
-                    var chave = evento.keyCode || evento.which;
-                    chave = String.fromCharCode(chave);
-                    var regex = /^[0-9.,]+$/;
-                    var regex = /^[0-9.]+$/;
-                    if (!regex.test(chave)) {
-                        evento.returnValue = false;
-                        if (evento.preventDefault) evento.preventDefault();
-                    }</script>
+                <input type="text" name="nos" id="nos" class="Caixa-Texto-N-OS" onkeypress='numeroOS()' required="required" />
             </div>
             <div>
                 <h2>Nome do Prestador de Serviço:</h2>
-                <input type="text" name="nome" id="nome" class="Caixa-Texto-Nome-Prestador"  required />
+                <input type="text" name="nome" id="nome" class="Caixa-Texto-Nome-Prestador" required />
             </div>
             <div>
                 <h2>Endereço:</h2>
-                <input type="text" name="endereco" id="endereco" class="Caixa-Texto-Endereco"  required />
+                <input type="text" name="endereco" id="endereco" class="Caixa-Texto-Endereco" required />
             </div>
             <div class="row">
                 <div class="column">
                     <h2>Número:</h2>
-                    <input type="text" name="numero" id="numero" class="Caixa-Texto-Numero-Casa"
-                          required />
-                    <script>var evento = evt || window.event;
-                        var chave = evento.keyCode || evento.which;
-                        chave = String.fromCharCode(chave);
-                        var regex = /^[0-9.,]+$/;
-                        var regex = /^[0-9.]+$/;
-                        if (!regex.test(chave)) {
-                            evento.returnValue = false;
-                            if (evento.preventDefault) evento.preventDefault();
-                        }</script>
+                    <input type="text" name="numero" id="numero" class="Caixa-Texto-Numero-Casa" onkeypress='numeroCasa();' required />
                 </div>
                 <div class="column">
                     <h2>Bairro:</h2>
-                    <input type="text" name="bairro" id="bairro" class="Caixa-Texto-Bairro"  required />
+                    <input type="text" name="bairro" id="bairro" class="Caixa-Texto-Bairro" required />
                 </div>
             </div>
             <div class="row">
                 <div class="column">
                     <h2>Cidade:</h2>
-                    <input type="text" name="cidade" id="cidade" class="Caixa-Texto-Cidade"  required />
+                    <input type="text" name="cidade" id="cidade" class="Caixa-Texto-Cidade" required />
                 </div>
                 <div class="columnEstado">
                     <h2>Estado:</h2>
-                    <select type="text" name="estado" id="estado" name="estado" class="Caixa-Texto-Estado"
-                        style="font-size: medium;" required >
+                    <select type="text" name="estado" id="estado" name="estado" class="Caixa-Texto-Estado" style="font-size: medium;" required>
                         <option selected="" value="">Selecione o Estado (UF)</option>
                         <option value="AC">Acre</option>
                         <option value="AL">Alagoas</option>
@@ -104,29 +83,17 @@
                 </div>
                 <div class="columnCEP">
                     <h2>CEP:</h2>
-                    <input type="text" name="CEP" id="cep" class="Caixa-Texto-CEP" placeholder="00000-000" size="10"
-                        maxlength="9" onkeypress="$(this).mask('00000-000')"  required />
+                    <input type="text" name="CEP" id="cep" class="Caixa-Texto-CEP" placeholder="00000-000" size="10" maxlength="9" onkeypress="$(this).mask('00000-000')" required />
                 </div>
             </div>
             <div class="row">
                 <div class="column">
                     <h2>Telefone (opicional):</h2>
-                    <input type="text" name="telefone" id="telefone" class="Caixa-Texto-Telefone"
-                        placeholder="(DDD)0000-0000" style="margin: auto;"
-                        onkeypress="$(this).mask('(00)00000-0000')"  />
+                    <input type="text" name="telefone" id="telefone" class="Caixa-Texto-Telefone" placeholder="(DDD)0000-0000" style="margin: auto;" onkeypress="$(this).mask('(00)00000-0000')" />
                 </div>
                 <div class="columnCpfCnpj">
                     <h2>CPF ou CNPJ:</h2>
-                    <input type="text" name="CpfCnpj" id="CpfCnpj" class="Caixa-Texto-CPF-CNPJ1"
-                        onkeypress="$(this).mask(CpfCnpjMask, cpfCnpjpOpcao)"  required />
-                    <script>var CpfCnpjMask = function (val) {
-                            return val.replace(/\D/g, '').length <= 11 ? '000.000.000-000' : '00.000.000/0000-00';
-                        },
-                            cpfCnpjpOpcao = {
-                                onKeyPress: function (val, e, field, options) {
-                                    field.mask(CpfCnpjMask.apply({}, arguments), options);
-                                }
-                            };</script>
+                    <input type="text" name="CpfCnpj" id="CpfCnpj" class="Caixa-Texto-CPF-CNPJ1" onkeypress="$(this).mask(CpfCnpjMask, cpfCnpjpOpcao)" required />
                     <sub>Somente os números</sub>
                 </div>
             </div>
@@ -137,20 +104,16 @@
             </head>
             <div>
                 <h2>Cliente:</h2>
-                <input type="text" name="cliente" id="cliente" class="Caixa-Texto-Cliente"  required />
+                <input type="text" name="cliente" id="cliente" class="Caixa-Texto-Cliente" required />
             </div>
             <div class="row">
                 <div class="column">
                     <h2>Telefone (opcional):</h2>
-                    <input type="text" name="telefone2" id="telefone2" class="Caixa-Texto-Telefone"
-                        placeholder="(DDD)0000-0000" style="margin: auto;"
-                        onkeypress="$(this).mask('(00)00000-0000')" />
+                    <input type="text" name="telefone2" id="telefone2" class="Caixa-Texto-Telefone" placeholder="(DDD)0000-0000" style="margin: auto;" onkeypress="$(this).mask('(00)00000-0000')" />
                 </div>
                 <div class="columnCpfCnpj">
                     <h2>CPF ou CNPJ(opicional):</h2>
-                    <input type="text" name="CpfCnpj2" id="CpfCnpj2" class="Caixa-Texto-CPF-CNPJ2"
-                        onkeypress="$(this).mask(CpfCnpjMask, cpfCnpjpOpcao)" />
-                        
+                    <input type="text" name="CpfCnpj2" id="CpfCnpj2" class="Caixa-Texto-CPF-CNPJ2" onkeypress="$(this).mask(CpfCnpjMask, cpfCnpjpOpcao)" />
                     <sub>Somente os números</sub>
                 </div>
             </div>
@@ -160,42 +123,34 @@
             </div>
             <div>
                 <h2>Descrição do Serviço:</h2>
-                <textarea name="descricao" id="descricao" cols="30" rows="10"
-                    placeholder="Descreva o problema relatado ou o serviço que será realizado"
-                    class="Caixa-Texto-Descricao" required ></textarea>
+                <textarea name="descricao" id="descricao" cols="30" rows="10" placeholder="Descreva o problema relatado ou o serviço que será realizado" class="Caixa-Texto-Descricao" required></textarea>
             </div>
             <div>
                 <h2>Serviço Executado:</h2>
-                <textarea name="servico" id="servico" cols="30" rows="10"
-                    placeholder="Descreva o serviço que será realizado" class="Caixa-Texto-Servico" required ></textarea>
+                <textarea name="servico" id="servico" cols="30" rows="10" placeholder="Descreva o serviço que será realizado" class="Caixa-Texto-Servico" required></textarea>
             </div>
             <div class="row">
                 <div class="column">
                     <h2>Data:</h2>
-                    <input id="data" class="Caixa-Data" type="date" maxlength="10" name="data" pattern="[0-9]{2}\/[0-9]{2}\/[0-9]{4}$"  required>
+                    <input id="data" class="Caixa-Data" type="date"="" maxlength="10" name="data" pattern="[0-9]{2}\/[0-9]{2}\/[0-9]{4}$" required>
                 </div>
 
                 <div class="column">
                     <h2>Hora:</h2>
-                    <input id="hora" class="Caixa-Hora" type="time"
-                    maxlength="8" name="hora" pattern="[0-9]{2}:[0-9]{2} [0-9]{2}$" required>
+                    <input id="hora" class="Caixa-Hora" type="time" maxlength="8" name="hora" pattern="[0-9]{2}:[0-9]{2} [0-9]{2}$" required>
                     <sub>Somente os números</sub>
                 </div>
             </div>
             <br>
             <div>
-                <button name="botao" class="Botao-GerarOS" onclick="ordemservico.p">
-                    <i class="fa-solid fa-check" style=" color: rgb(19, 16, 48);font-size: x-large;">
-                    </i>
-                    <b class="b"> GerarO.S</b>
-                </button>
+                <input type="submit" name="botao" class="Botao-GerarOS" value="Gerar O.S" />
             </div>
             <br>
 
         </form>
     </div>
-</div>
-<div id="ordem"></div>
+
+    
     <script src="script.js"></script>
 </body>
 
