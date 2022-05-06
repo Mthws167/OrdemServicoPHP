cbedac8939f86d44c6edc465649f369f8ffb2910
Matheus Henrique de Oliveria <62122558+Mthws167@users.noreply.github.com>
2022-05-06 20:18:00 -0300

    Update ordemservico.php

--- ordemservico.php
+++ ordemservico.php
@@ -20,51 +20,49 @@
 
 
 <body class="box">
-    <?php   
 
-    echo "<div id='form2' style='padding-right: 10px'>";
-    echo "<div>";
-    echo "<p>" . $_POST["data"] . " |  " . $_POST["hora"] . "</p>";
-    echo "</div>";
-    echo "<br>" . "<br>";
-    echo "<div style=border:solid 2px;>";
-    echo "<p>" . $_POST["nome"] . "</p>";
-    echo "<p>" . "CPF/CNPJ: " . $_POST["CpfCnpj"] . "</p>";
-    echo "<p>" . $_POST["cidade"] . "," . $_POST["estado"] . "," . "CEP:" . $_POST["CEP"] . "</p>";
-    echo "<p>" . $_POST["endereco"] . "," . $_POST["numero"] . "," . $_POST["bairro"] . "</p>";
-    echo "<p>" . $_POST["telefone"] . "</p>";
-    echo "</div>";
-    echo "<br>" . "<br>";
-    echo "<div style=text-align:center;>" . "ORDEM DE SERVIÇO Nº: " . $_POST["nos"] . "</div>";
-    echo "<div>";
-    echo "<p>" . "CLIENTE: " . $_POST["cliente"] . "</p>";
-    echo "<p>" . "CPF/CNPJ: " . $_POST["CpfCnpj2"] . "</p>";
-    echo "<p>" . "TELEFONE: " . $_POST["telefone2"] . "</p>" . "<br>";
-    echo "Obs.: " . $_POST["obs"];
-    echo "</div>";
-    echo "<div style=border:solid 2px;>";
-    echo "DESCRIÇÃO DO SERVIÇO/DEFEITO RELATADO:";
-    echo "<p>" . $_POST["descricao"] . "</p>";
-    echo "</div>" . "<br>";
-    echo "<div style=border:solid 2px;>";
-    echo "SERVIÇO EXECUTOADO:";
-    echo "<p>" . $_POST["servico"] . "</p>";
-    echo "</div>";
-    echo "<br>" . "<br>" . "<br>" . "<br>" . "<br>";
-    echo "<div class='row' style='padding-left:170px;'>";
-    echo "<div class='column'>";
-    echo "<hr>";
-    echo " <p>" . "Assinatura do Cliente" . "</p>";
-    echo " </div>";
-    echo " <div class='column' style='padding-left: 50px;'>";
-    echo " <hr>";
-    echo " <p>" . "Técnico Responsável" . "</p>";
-    echo " </div>";
-    echo "</div>";
-    echo "<br>" . "<br>" . "<br>" . "<br>";
-    echo "</div>";
+     <div id='form2' style='padding-right: 10px'>
+     <div>
+     <p> <?=$_POST["data"]?>  |   <?=$_POST["hora"]?> </p>
+     </div>
+     <br> <br>
+     <div style=border:solid 2px>
+     <p><?=$_POST["nome"]?> </p>
+     <p> CPF/CNPJ: <?=$_POST["CpfCnpj"]?> </p>
+     <p><?=$_POST["cidade"]?> ,<?=$_POST["estado"]?> , CEP:<?=$_POST["CEP"]?> </p>
+     <p><?=$_POST["endereco"]?> ,<?=$_POST["numero"]?> ,<?=$_POST["bairro"]?> </p>
+     <p><?=$_POST["telefone"]?> </p>
+     </div>
+     <br> <br>
+     <div style=text-align:center> ORDEM DE SERVIÇO Nº: <?=$_POST["nos"]?> </div>
+     <div>
+     <p> CLIENTE: <?=$_POST["cliente"]?> </p>
+     <p> CPF/CNPJ: <?=$_POST["CpfCnpj2"]?> </p>
+     <p> TELEFONE: <?=$_POST["telefone2"]?> </p> <br>
+     <p>Obs.: <?=$_POST["obs"]?></p>
+     </div>
+     <div style=border:solid 2px>
+     DESCRIÇÃO DO SERVIÇO/DEFEITO RELATADO:
+     <p><?=$_POST["descricao"]?> </p>
+     </div> <br>
+     <div style=border:solid 2px>
+     SERVIÇO EXECUTOADO:
+     <p><?=$_POST["servico"]?> </p>
+     </div>
+     <br> <br> <br> <br> <br>
+     <div class='row' style='padding-left:170px'>
+     <div class='column'>
+     <hr>
+      <p> Assinatura do Cliente </p>
+      </div>
+      <div class='column' style='padding-left: 50px'>
+      <hr>
+      <p> Técnico Responsável </p>
+      </div>
+     </div>
+     <br> <br> <br> <br>
+     </div>
     
-    ?>
     <div style="padding-left:300px" ;>
         <button  class="Botao-GerarOS" onclick="imprimir()">
             <i class="fa-solid fa-file" style="color: rgb(19, 16, 48);font-size: x-large"></i>
